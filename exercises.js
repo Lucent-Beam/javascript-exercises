04a02b70cc39d843ca94a4c31a78208ef1a2c03c
Luxth <luis.charres.06@gmail.com>
2020-04-04 22:44:00 +0100

    Added other some exercises more

--- exercises.js
+++ exercises.js
@@ -131,4 +131,101 @@ decodeMorse = function(morseCode){
 
  console.log(sumTwoLessNumbers([10,20,1,15]));
 
- 
+
+ /*Find numbers between two numbers given. Details:
+    a = start, b = end ... (1,10) => 1,2,3,4,5,6,7,8,10
+    but these numbers must comply the following:
+
+    89 => 8^1 + 9^2 = 89!!!
+    2 => 2^1 = 2!!!
+
+    then a real example would be something like this:
+
+    sumDigPow(1,150) => [1,2,3,4,5,6,7,9,89,135];
+
+ */
+
+sumDigPow = function(a,b){
+
+    let _newArray = [], _numberFound;
+
+    while (a<=b) {
+        _numberFound = a.toString().split('').reduce((x,y,index) => {
+            return parseInt(x) + parseInt(Math.pow(y, index+1));
+        });
+
+        if (_numberFound == a) {
+            _newArray.push(a);
+        }
+
+        a++;
+    }
+
+    return _newArray;
+
+}
+
+
+console.log(sumDigPow(1,150));
+
+
+
+/* Order odd numbers from a given array, buth without change the place of even numbers. 
+    Example: [5,4,6,1,2] => [1,4,6,5,2];
+
+*/
+
+order_odd_numbers = function(arr){
+
+    let _oddNumbersArrOrdered = [], _counter = -1;
+
+    _oddNumbersArrOrdered = arr.filter((c) => c%2 != 0).sort((a,b) => a-b);
+
+    return arr.map((c, index) => {
+
+        if(c%2 != 0){
+            _counter++;
+            return _oddNumbersArrOrdered[_counter];
+        }
+        return c;
+
+    });
+}
+
+console.log(order_odd_numbers([5,4,6,1,2]));
+
+
+/*
+
+*/
+
+function list(names){
+    //your code here
+     function extract(c){
+       return c.name;
+     }
+    
+    names = names.map(extract);
+    
+    return names.slice(0, names.length -2).join(", ")+", ".repeat((names.length >= 3) ? 1 : 0)+names.slice(-2).join(" & ");
+  }
+
+  console.log(list([{name: 'Bart'},{name: 'Luis'}]))
+
+
+    lister = function(names){
+        return names.reduce((prev, current, index, arr) => {
+
+            if (index === 0) {
+                return current.name;
+            }
+
+            if(index == names.length - 1){
+                return prev + " & " + current.name;
+            }
+
+            return prev + " , " + current.name;
+        }, '');
+  }
+
+  console.log(lister([{name: 'Bart'},{name: 'Luis'}, {name: 'Claudia'}]))
